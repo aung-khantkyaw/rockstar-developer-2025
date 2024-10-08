1fcaa86ed2ffbcebb66d73e0797fe13709a78719
aung-khantkyaw <aungkhantkyaw.19-20@ucsmub.edu.mm>
2024-10-08 21:47:04 +0630

    [Finished] Chapter 6

--- yaycha/src/ThemedApp.jsx
+++ yaycha/src/ThemedApp.jsx
@@ -1,13 +1,41 @@
-import { createContext, useState } from "react";
+// import { createContext, useState } from "react";
+// import App from "./App";
+
+// export const AppContext = createContext();
+
+// export default function ThemedApp() {
+//   const [mode, setMode] = useState("dark");
+//   return (
+//     <AppContext.Provider value={{ mode, setMode }}>
+//       <App />
+//     </AppContext.Provider>
+//   );
+// }
+
+// ################################################### //
+
+// Chapter6 - React UI Framework MUI
+
+import { useState, createContext, useContext } from "react";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import App from "./App";
-
-export const AppContext = createContext();
-
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+const AppContext = createContext();
+export function useApp() {
+  return useContext(AppContext);
+}
 export default function ThemedApp() {
-  const [mode, setMode] = useState("dark");
+  const [showForm, setShowForm] = useState(false);
   return (
-    <AppContext.Provider value={{ mode, setMode }}>
-      <App />
-    </AppContext.Provider>
+    <ThemeProvider theme={theme}>
+      <AppContext.Provider value={{ showForm, setShowForm }}>
+        <App />
+        <CssBaseline />
+      </AppContext.Provider>
+    </ThemeProvider>
   );
 }
